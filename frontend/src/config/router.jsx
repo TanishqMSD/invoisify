e17bdfdde7ee9bac250fa7b49d32a0df20d533b6
Mohitd45 <mohit.dhangar88@gmail.com>
2024-12-28 14:51:58 +0530

    routes made

--- frontend/src/config/router.jsx
+++ frontend/src/config/router.jsx
@@ -1,14 +1,14 @@
 import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-  } from "react-router-dom";
-  import App from "../App.jsx";
-  import NotFound from "../pages/NotFound.jsx";
-  import Home from "../pages/Home.jsx";
-  import ForgotPassword from "../pages/ForgotPassword.jsx";
-  import Login from "../pages/Login.jsx";
-  import Invoices from "../pages/Invoices.jsx"
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import App from "../App.jsx";
+import NotFound from "../pages/NotFound.jsx";
+import Home from "../pages/Home.jsx";
+import ForgotPassword from "../pages/ForgotPassword.jsx";
+import Login from "../pages/Login.jsx";
+import Invoices from "../pages/Invoices.jsx"
 import Register from "../pages/Register.jsx";
 import InvoiceMaker from "../pages/InvoiceMaker.jsx";
 import InvoicePreview from "../pages/InvoicePreview.jsx";
@@ -34,27 +34,4 @@ import Dashboard from "../pages/Dashboard.jsx";
   );
   
   export default router;
-  
-=======
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<App />}>
-        <Route path="/home" element={<Home />} />
-      </Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/invoices" element={<Invoices />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-
-      <Route path="*" element={<NotFound />} />
-      <Route path="/invoice-maker" element={<InvoiceMaker />} />
-      <Route path="/invoice-preview" element={<InvoicePreview />} />
-    </>
-  )
-);
-
-export default router;
->>>>>>> 66872817022b06d4d1c8ed98ea12de628e7f9a29
   
