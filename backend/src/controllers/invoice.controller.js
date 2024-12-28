66872817022b06d4d1c8ed98ea12de628e7f9a29
TanishqMSD <tanishqkulkarni18@gmail.com>
2024-12-28 12:13:48 +0530

    minor

--- backend/src/controllers/invoice.controller.js
+++ backend/src/controllers/invoice.controller.js
@@ -30,6 +30,36 @@ const createInvoice = asyncHandler(async (req, res) => {
         new ApiResponse(201, newInvoice, "Invoice created successfully")
     );
 
+    const createTemplate = asyncHandler(async (req, res) => {
+        const { companyName, billingAddress, backgroundColor } = req.body;
+        
+        if (!companyName?.trim() || !billingAddress?.trim() || !backgroundColor?.trim()) {
+            throw new ApiError(400, "All fields are required");
+        }
+        
+        const newTemplate = await Template.create({
+            companyName,
+            billingAddress,
+            backgroundColor,
+        });
+        
+        if (!newTemplate) {
+            throw new ApiError(500, "Something went wrong");
+        }
+        
+        return res.status(201).json(
+            new ApiResponse(201, newTemplate, "Template created successfully")
+        );
+    });
+    
+    const getTemplates = asyncHandler(async (req, res) => {
+        const templates = await Template.find();
+        
+        return res.status(200).json(
+            new ApiResponse(200, templates, "Templates retrieved successfully")
+        );
+    })
+
 });
 
-export { createInvoice };
+export { createInvoice, createTemplate, g };
--- frontend/src/config/router.jsx
+++ frontend/src/config/router.jsx
@@ -1,35 +1,34 @@
 import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-  } from "react-router-dom";
-  import App from "../App.jsx";
-  import NotFound from "../pages/NotFound.jsx";
-  import Home from "../pages/Home.jsx";
-  import ForgotPassword from "../pages/ForgotPassword.jsx";
-  import Login from "../pages/Login.jsx";
-  import Invoices from "../pages/Invoices.jsx"
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import App from "../App.jsx";
+import NotFound from "../pages/NotFound.jsx";
+import Home from "../pages/Home.jsx";
+import ForgotPassword from "../pages/ForgotPassword.jsx";
+import Login from "../pages/Login.jsx";
+import Invoices from "../pages/Invoices.jsx"
 import Register from "../pages/Register.jsx";
 import InvoiceMaker from "../pages/InvoiceMaker.jsx";
 import InvoicePreview from "../pages/InvoicePreview.jsx";
-  
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/" element={<App />}>
-          <Route path="/home" element={<Home />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/invoices" element={<Invoices/>} />
-        <Route path="/forgot-password" element={<ForgotPassword/>} />
-        
-        <Route path="*" element={<NotFound />} />
-        <Route path="/invoice-maker" element={<InvoiceMaker />} />
-        <Route path="/invoice-preview" element={<InvoicePreview />} />
-      </>
-    )
-  );
-  
-  export default router;
-  
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<App />}>
+        <Route path="/home" element={<Home />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/invoices" element={<Invoices />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+
+      <Route path="*" element={<NotFound />} />
+      <Route path="/invoice-maker" element={<InvoiceMaker />} />
+      <Route path="/invoice-preview" element={<InvoicePreview />} />
+    </>
+  )
+);
+
+export default router;
